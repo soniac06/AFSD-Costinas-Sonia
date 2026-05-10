30a4195af3a664e8c6ff33f1dfc0493cf39840c7
soniac06 <soniacostinas@gmail.com>
2026-05-10 21:20:33 +0300

    Laborator 6

--- L6_3.py
+++ L6_3.py
@@ -19,44 +19,44 @@ for nume, cost, scor in candidati:
     print(f"{nume:<10} cost={cost:<5} scor={scor}")
 print("\nBuget maxim:", BUGET_MAXIM)
 print("-" * 40)
-NUMAR_GENE=len(candidati)
-POPULATIE=50
-GENERATII=100
-RATA_MUTATIE=0.05
-def cromozom_random():
-    return [random.randint(0, 1) for _ in range(NUMAR_GENE)]
+NUMAR_GENE=len(candidati) #numărul de candidați
+POPULATIE=50 #Numărul de soluții existente într-o generație
+GENERATII=100 #Numărul de iterații ale algoritmului genetic
+RATA_MUTATIE=0.05 #Probabilitatea ca o genă să fie modificată
+def genereaza_cromozom():
+    return [random.randint(0, 1) for _ in range(NUMAR_GENE)] #Creează o soluție aleatoare
 
-def fitness(cromozom):
+def fitness(cromozom): #Evaluează cât de bună este o soluție
     cost_total=0
     scor_total=0
     for gena, candidat in zip(cromozom, candidati):
         nume, cost, scor=candidat
-        if gena==1:
+        if gena==1: #înseamnă că acel candidat este selectat
             cost_total+=cost
             scor_total+=scor
     if cost_total>BUGET_MAXIM:
         return 0
     return scor_total
 
-def selectie(populatie):
+def selectie(populatie): #Alege părinți pentru reproducere
     a=random.choice(populatie)
     b=random.choice(populatie)
     if fitness(a)>fitness(b):
         return a
     return b
 
-def crossover(parinte1, parinte2):
+def incrucisare(parinte1, parinte2): #Combină doi părinți pentru a produce un copil
     punct=random.randint(1, NUMAR_GENE-1)
     copil=parinte1[:punct]+parinte2[punct:]
     return copil
 
-def mutatie(cromozom):
+def mutatie(cromozom): #Introduce modificări aleatoare
     for i in range(NUMAR_GENE):
         if random.random()<RATA_MUTATIE:
             cromozom[i]=1-cromozom[i]
 
-populatie=[cromozom_random() for _ in range(POPULATIE)]
-best_global=max(populatie, key=fitness)
+populatie=[genereaza_cromozom() for _ in range(POPULATIE)] #Generează 50 soluții aleatoare
+best_global=max(populatie, key=fitness) #Memorează cea mai bună soluție găsită până în acel moment
 istoric_best=[]
 istoric_medie=[]
 for generatie in range(GENERATII):
@@ -72,7 +72,7 @@ for generatie in range(GENERATII):
     for _ in range(POPULATIE):
         p1=selectie(populatie)
         p2=selectie(populatie)
-        copil=crossover(p1, p2)
+        copil=incrucisare(p1, p2)
         mutatie(copil)
         populatie_noua.append(copil)
     populatie=populatie_noua
